9278d0af77ebcd292d2a2961a576fe356c80c686
Andrei Balaban <andreibalaban201@gmail.com>
2024-02-23 11:32:50 +0200

    product crud

--- src/api/products/index.ts
+++ src/api/products/index.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { supabase } from "@/lib/supabase";
 
 export const useProductList = () => {
@@ -24,5 +24,66 @@ export const useProduct = (id: number) => {
       }
       return data;
     },
+  });
+};
+
+export const useInsertProduct = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    async mutationFn(data: any){
+      const {error, data: newProduct} = await supabase.from('products').insert({
+        name: data.name,
+        image: data.image,
+        price: data.price
+      }).single();
+
+      if(error){
+        throw new Error(error.message);
+      }
+      return newProduct;
+    },
+    async onSuccess() {
+      await queryClient.invalidateQueries(['products']);
+    },
+  });
+};
+
+export const useUpdateProduct = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    async mutationFn(data: any){
+      const {error, data: updatedProduct} = await supabase.from('products').update({
+        name: data.name,
+        image: data.image,
+        price: data.price
+      }).eq('id', data.id).select().single();
+
+      if(error){
+        throw new Error(error.message);
+      }
+      return updatedProduct;
+    },
+    async onSuccess(_, {id}) {
+      await queryClient.invalidateQueries(['products']);
+      await queryClient.invalidateQueries(['products', data.id]);
+    },
+  });
+};
+
+export const useDeleteProduct = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    async mutationFn(id: number){
+      const {error} = await supabase.from('products').delete().eq('id', id);
+      if(error){
+        throw new Error(error.message);
+      }
+    },
+    async onSuccess() {
+      await queryClient.invalidateQueries(['products']);
+    }
   });
 };
--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,10 +1,11 @@
 import { View, Text, StyleSheet, TextInput, Image, Alert} from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '@/components/Button';
 import { defaultPizzaImage } from '@/components/ProductListItem';
 import Colors from '@/constants/Colors';
 import * as ImagePicker from 'expo-image-picker';
-import { Stack, useLocalSearchParams } from 'expo-router';
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
+import { useDeleteProduct, useInsertProduct, useProduct, useUpdateProduct } from '@/api/products';
 
 const CreateProductScreen = () => {
     const [name, setName] = useState('');
@@ -12,9 +13,26 @@ const CreateProductScreen = () => {
     const [errors, setErrors] = useState('');
     const [image, setImage] = useState<string | null>(null);
 
-    const { id } = useLocalSearchParams();
-    const isUpdating = !!id;
+    const { id: idString } = useLocalSearchParams();
+    const id = parseFloat(typeof idString === 'string' ? idString : idString?.[0]);
+    
+    const isUpdating = !!idString;
+
+    const { mutate: insertProduct } = useInsertProduct();
+    const { mutate: updateProduct } = useUpdateProduct();
+    const {data: updatingProduct} = useProduct(id);
+    const {mutate: deleteProduct} = useDeleteProduct();
+
+    const router = useRouter();
   
+    useEffect(() => {
+        if(updatingProduct){
+            setName(updatingProduct.name);
+            setPrice(updatingProduct.price.toString());
+            setImage(updatingProduct.image);
+        }
+    }, [updatingProduct]);
+
     const resetFields = () => {
         setName('');
         setPrice('');
@@ -42,7 +60,7 @@ const CreateProductScreen = () => {
 
     const onSubmit = () => {
         if(isUpdating){
-            onUpdateCreate();
+            onUpdate();
         }else{
             onCreate();
         }
@@ -53,19 +71,26 @@ const CreateProductScreen = () => {
             return;
         }
 
-        console.warn('Creating product ', name);
-
-        resetFields();
+        insertProduct({name, price: parseFloat(price), image}, {
+            onSuccess: () => {
+                resetFields();
+                router.back();
+            }
+        });
     };
 
-    const onUpdateCreate = () => {
+    const onUpdate = () => {
         if(!validateInput()){
             return;
         }
 
-        console.warn('Updating product ', name);
-
-        resetFields();
+        updateProduct({id, name, price: parseFloat(price), image}, 
+        {
+            onSuccess: () => {
+                resetFields();
+                router.back();
+            }
+        });
     };
 
     const pickImage = async () => {
@@ -84,7 +109,12 @@ const CreateProductScreen = () => {
     };
 
     const onDelete = () => {
-        console.warn('Delete');
+        deleteProduct(id, {
+            onSuccess: () => {
+                resetFields();
+                router.replace('/(admin)');
+            }
+        });
     };
 
     const confirmDelete = () => {
