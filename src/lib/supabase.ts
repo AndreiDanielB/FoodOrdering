d9a30cafa4ba88a7b4324753b8ae940e005250a3
Andrei Balaban <andreibalaban201@gmail.com>
2024-02-25 10:52:39 +0200

    supabase local setup

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,25 +4,27 @@ import { createClient } from '@supabase/supabase-js';
 import { Database } from '@/database.types';
 
 const ExpoSecureStoreAdapter = {
-    getItem: (key: string) => {
-        return SecureStore.getItemAsync(key);
-    },
-    setItem: (key: string, value: string) => {
-        SecureStore.setItemAsync(key, value);
-    },
-    removeItem: (key: string) => {
-        SecureStore.deleteItemAsync(key);
-    },
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
 };
 
-const supabaseUrl = 'https://dzagczqiubypyzvpjnue.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6YWdjenFpdWJ5cHl6dnBqbnVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1OTQ5MjQsImV4cCI6MjAyNDE3MDkyNH0.O6d0J464UdV9QTlc99k7P_LcjcPqle4JDuwIKw-iDuE';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON || '';
+
+console.log(supabaseUrl, supabaseAnonKey);
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
-    auth: {
-        storage: ExpoSecureStoreAdapter as any,
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: false,
-    },
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
 });
